256145681225cd3faedb7f8457edc82d5b01244e
Harikrishnan <harikrishnan8421@gmail.com>
2020-07-31 17:05:37 +0530

    Initial commit

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,29 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.pluralsight.blog.model.Post;
+
+import com.pluralsight.blog.data.PostRepository;
 
 @Controller
 public class BlogController {
-	
+	@Autowired
+	private PostRepository postRepository;
 	@RequestMapping("/")
 	public String listPosts(ModelMap map) {
+		List<Post> pList=postRepository.getAllPosts();
 		map.put("title", "Blog Post 1");
+		map.put("posts", pList);
 		return "home";
+	}
+	
+	public BlogController(PostRepository postRepository) {
+		this.postRepository=postRepository;
 	}
 
 }
